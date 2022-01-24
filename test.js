f87a6089589b43db7984ef8b24edabb5d48479a5
ouaksel <ouakselboukhalfa.ob@gmail.com>
2022-01-24 22:45:16 +0100

    fusion_deux

--- test.js
+++ test.js
@@ -1,5 +1,25 @@
-const a = 12;
-let b = 45;
 
-b += a ;
-console.log("resultat de a + b = " + b);
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Dimanche";
+    break;
+  case 1:
+    day = "Lundi";
+    break;
+  case 2:
+     day = "Mardi";
+    break;
+  case 3:
+    day = "Mercredi";
+    break;
+  case 4:
+    day = "Jeudi";
+    break;
+  case 5:
+    day = "Vendredi";
+    break;
+  case 6:
+    day = "Samedi";
+}
+console.log("Aujourd'hui nous sommes " + day);
