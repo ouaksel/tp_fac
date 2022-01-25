ce34b28a4b2fe13a5c64b9be7478ea3a9bd05f9c
ouaksel <ouakselboukhalfa.ob@gmail.com>
2022-01-25 09:23:16 +0100

    insertion_date

--- test.js
+++ test.js
@@ -1,25 +1,21 @@
+var j = new Date();
+var jourSemaine = ['dimanche','lundi','mardi','mercredi','jeudi','vendredi','samedi'];
+var mois = ['janvier','fÃ©vrier','mars','avril','mai','juin',
+            'juillet','aoÃ»t','septembre','octobre','novembre','dÃ©cembre'];
 
-let day;
-switch (new Date().getDay()) {
-  case 0:
-    day = "Dimanche";
-    break;
-  case 1:
-    day = "Lundi";
-    break;
-  case 2:
-     day = "Mardi";
-    break;
-  case 3:
-    day = "Mercredi";
-    break;
-  case 4:
-    day = "Jeudi";
-    break;
-  case 5:
-    day = "Vendredi";
-    break;
-  case 6:
-    day = "Samedi";
+function aujourdHui(){
+   return jourSemaine[j.getDay()]+' '+j.getDate()+' '+
+          mois[j.getMonth()]+' '+j.getFullYear()+' '+
+          j.getHours()+":"+(j.getMinutes()<10?"0":"")+j.getMinutes();
+}            
+
+function insererDate(){
+    var dateElem = document.getElementById("date");
+    var b = document.createElement("b");
+    b.appendChild(document.createTextNode(aujourdHui()));
+    dateElem.appendChild(b);
 }
-console.log("Aujourd'hui nous sommes " + day);
+
+// une meilleure alternative Ã  l'utilisation de l'attribut "onload='insererDate()'" du body
+
+window.addEventListener("load",insererDate,false);
